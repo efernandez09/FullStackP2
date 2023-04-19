9029ef7e380d39e6db47041a8f2a4afee15db9ba
EriCode09 <efernandezperez0@uoc.edu>
2023-04-19 16:43:11 +0200

    Servidor Apollo Operativo (Types definidos, faltan Querys y Resolvers y como vincularalas a la bbdd)

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
 const express = require('express');
-const app = express();
 
-// Importamos las rutas de las Tasks y las Cards
+// Importamos apollo server (Importamos los typeDefs y los resolvers)
+const { ApolloServer } = require('apollo-server-express');
+const typeDefs = require('../FullStackP2/src/models/typeDefs');
+const resolvers = require('../FullStackP2/src/controllers/resolvers');
+
+// Importamos las rutas de las Tasks y las Cards (No necesario de momento)
 const cardRoutes = require('../FullStackP2/src/routes/cardRoutes');
 const taskRoutes = require('../FullStackP2/src/routes/taskRoutes');
 
@@ -15,20 +19,11 @@ const options = {
   useUnifiedTopology: true
 };
 
-// Conexión
+// ! Conexión a la base de datos
 mongoose.connect(uri, options);
 
 // Obtener la instancia de la conexión
 const db = mongoose.connection;
-
-
-// Cors
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
-
 
 // Manejar eventos de la conexión
 db.on('error', console.error.bind(console, 'Error de conexión:'));
@@ -37,12 +32,41 @@ db.once('open', function() {
   // hacer algo con la base de datos
 });
 
-// Definimos el pueto predeterminado y lo que se ejecutara cuando se inicie el servidor.
-app.listen(3000, function() {
-  console.log('Servidor iniciado en http://localhost:3000');
-});
+// Funcion para iniciar el servidor Apollo-Express
+async function startServer() {
+  
+  const app = express();
 
-// Middelwares
-app.use('/api/cards', cardRoutes);
-app.use('/api/tasks', taskRoutes);
-app.use(express.static('public'));
+  // Declaraciones app.use EN CASO DE NO FUNCIONAR PROBAR A DECLARAR DESPUES DEL MIDDELWARE DE APOLLO
+  
+  // Cors
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
+
+  // Middelwares (No neces)
+  app.use('/api/cards', cardRoutes);
+  app.use('/api/tasks', taskRoutes);
+  app.use(express.static('public'));
+
+
+  const server = new ApolloServer({typeDefs, resolvers});
+
+  // Se debe declarar esta funcion asíncrona para evitar que el middelware
+  // que une apollo con express se aplique antes de que se inicie el servicio y cause errores
+  await server.start();
+
+  // Unimos apollo server a la aplicacion de express
+  server.applyMiddleware({app});
+  
+  // Definimos el pueto predeterminado y lo que se ejecutara cuando se inicie el servidor.
+  app.listen(3000, function() {
+    console.log('🚀 Frontend client corriendo en http://localhost:3000 🚀 ')
+    console.log(`🚀 Servidor de apollo en http://localhost:3000${server.graphqlPath} 🚀`)
+  });
+}
+
+// Iniciamos el servidor
+startServer();
